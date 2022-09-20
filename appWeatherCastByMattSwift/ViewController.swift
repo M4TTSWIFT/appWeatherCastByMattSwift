09c2b4a5e1564451677bfddefed321d239888431
Alex Petrovskyi <a.petrovskyi95@yahoo.com>
2022-09-20 18:03:01 +0300

    version 1.0.0 1. Finished with all preparations. 2. added images, labels into view 3. Navigation bar shows city names and added buttons

--- appWeatherCastByMattSwift/ViewController.swift
+++ appWeatherCastByMattSwift/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var currentweatherImage: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    @IBOutlet weak var windDirectionImage: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupNavigationBar()
     }
 
+    //MARK: - setup Navigation Bar:
 
+    private func setupNavigationBar() {
+        
+        navigationController?.navigationBar.tintColor = .white
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        UINavigationBar.appearance().standardAppearance = appearance
+        
+        let navigationButton = UIButton(type: .system)
+        navigationButton.setImage(#imageLiteral(resourceName: "ic_my_location"), for: .normal)
+        navigationButton.addTarget(self, action: #selector(navigationIconTapped), for: .touchUpInside)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: navigationButton)
+        
+        let navigationBarCityLabel = UILabel()
+        navigationBarCityLabel.textColor = .white
+        navigationBarCityLabel.text = "cityName"
+        
+        let placeMarkButton = UIButton(type: .system)
+        placeMarkButton.setImage(#imageLiteral(resourceName: "ic_place"), for: .normal)
+        placeMarkButton.addTarget(self, action: #selector(placeIconTapped), for: .touchUpInside)
+        
+        navigationItem.leftBarButtonItems = [UIBarButtonItem(customView: placeMarkButton),
+                                             UIBarButtonItem(customView: navigationBarCityLabel)
+                                            ]
+    }
+    
+    @objc func placeIconTapped() {
+        print("Place icon pressed.")
+    }
+    
+    @objc func navigationIconTapped() {
+        print("Navigation icon pressed.")
+    }
+    
 }
 
